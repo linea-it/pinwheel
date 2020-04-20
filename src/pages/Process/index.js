e1752d03b845ea23c6b32980eb5291a0b7445332
Fernando <fernando_nicolau@outlook.com>
2020-04-20 16:34:16 -0300

    feat: Components with process tabs.

--- src/pages/Process/index.js
+++ src/pages/Process/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import { useParams } from 'react-router-dom';
+import TabPanel from '../../components/TabPanel';
+import ProcessSummary from '../ProcessSumary';
 
 function Process() {
   const { id } = useParams();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <div>
-      <h1>{`Process ${id}`}</h1>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Process Summary" />
+          <Tab label="Results" />
+          <Tab label="Commensts" />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <ProcessSummary />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <h1>{`Results - Process ${id}`}</h1>
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <h1>{`Commensts - Process ${id}`}</h1>
+      </TabPanel>
     </div>
   );
 }
